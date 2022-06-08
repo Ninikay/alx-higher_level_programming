23f52388f8906cc7575aca959b87cc1daf6358ac
Ninikay <ms.nikeoni@gmail.com>
2022-06-08 19:50:02 +0000

    convert roman numerals to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    
+def roman_to_int(roman_string): 
     if roman_string is None or type(roman_string) is not str:
         return (0)
     roman_dict = {
