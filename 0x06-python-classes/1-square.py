f876e113cc901469288155b55e397e5e65b1c6ee
Ninikay <ms.nikeoni@gmail.com>
2022-06-21 14:40:11 +0000

    a class Square based on task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """ Square that defines a square 7object
+    Private instance attribute: size.
+    """
+
+    def __init__(self, size):
+        """Initializes the data that stores the size of the square ."""
+        self.__size = size
