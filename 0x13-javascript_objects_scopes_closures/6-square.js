ce65eb5b40e28e5ade992cd2cb1dc69be9d3daf2
Ninikay <ms.nikeoni@gmail.com>
2022-09-07 03:39:10 +0000

    updating squares

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
-const SquareBase = require('./5-square');
+const Square = require('./5-square');
 
-module.exports = class Square extends SquareBase {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square1 extends Square {
   charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write(c);
-      }
-      console.log();
+    if (!c) this.print();
+    else {
+      for (let i = 0; i < this.width; i++) console.log(c.repeat(this.height));
     }
   }
-};
+}
+
+module.exports = Square1;
